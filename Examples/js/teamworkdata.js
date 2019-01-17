fc41c2802db87da010dcbe7f61c91bd9bc55d61f
Courtenay <hdsheena@users.noreply.github.com>
2019-01-17 11:27:12 -0800

    Update and rename earthquakeUSGS.js to teamworkdata.js

--- Examples/js/teamworkdata.js
+++ Examples/js/teamworkdata.js
@@ -8,21 +8,21 @@
             id: "id",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "mag",
-            alias: "magnitude",
-            dataType: tableau.dataTypeEnum.float
-        }, {
-            id: "title",
-            alias: "title",
+            id: "taskname",
+            alias: "taskname",
             dataType: tableau.dataTypeEnum.string
         }, {
-            id: "location",
-            dataType: tableau.dataTypeEnum.geometry
+            id: "duedate",
+            alias: "due date",
+            dataType: tableau.dataTypeEnum.string
+        }, {
+            id: "assignee",
+            dataType: tableau.dataTypeEnum.string
         }];
 
         var tableSchema = {
-            id: "earthquakeFeed",
-            alias: "Earthquakes with magnitude greater than 4.5 in the last seven days",
+            id: "tasklist",
+            alias: "Tasks from Teamwork",
             columns: cols
         };
 
@@ -31,17 +31,17 @@
 
     // Download the data
     myConnector.getData = function(table, doneCallback) {
-        $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson", function(resp) {
-            var feat = resp.features,
+        $.getJSON("https://smartflow.teamwork.com/tasks.json", function(resp) {
+            var feat = resp["todo-items"],
                 tableData = [];
 
             // Iterate over the JSON object
             for (var i = 0, len = feat.length; i < len; i++) {
                 tableData.push({
                     "id": feat[i].id,
-                    "mag": feat[i].properties.mag,
-                    "title": feat[i].properties.title,
-                    "location": feat[i].geometry
+                    "taskname": feat[i].description,
+                    "duedate": feat[i]["due-date"],
+                    "assignee": feat[i]["responsible-party-ids"]
                 });
             }
 
@@ -55,7 +55,7 @@
     // Create event listeners for when the user submits the form
     $(document).ready(function() {
         $("#submitButton").click(function() {
-            tableau.connectionName = "USGS Earthquake Feed"; // This will be the data source name in Tableau
+            tableau.connectionName = "Teamwork Tasks"; // This will be the data source name in Tableau
             tableau.submit(); // This sends the connector object to Tableau
         });
     });
